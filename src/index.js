dd3677395fa72347d67a8c67efeb8caf726ee683
Doug Martin <douglasemartin@gmail.com>
2011-08-04 17:21:01 -0400

    [ADDED] zork demo

--- src/index.js
+++ src/index.js
@@ -1,83 +1,89 @@
 $(function () {
 
-	var converter = new Showdown.converter(),
-		container = $("#container")[0],
-		quxFile = Quxly.File($("#qux")[0].innerHTML),
-		quxViewer;
+	try {
+		var converter = new Showdown.converter(),
+			container = $("#container")[0],
+			quxFile = Quxly.File($("#qux")[0].innerHTML),
+			quxViewer;
 
-	function closeModal() {
-		try {
-			$.modal.close();
+		function closeModal() {
+			try {
+				$.modal.close();
+			}
+			catch (e) {
+			}
 		}
-		catch (e) {
-		}
-	}
 
-	$(window).hashchange(function () {
-		var newStateName = window.location.hash.substr(1);
-		if (newStateName.length == 0) {
-			closeModal();
-			return;
-		}
-		
-		if (!quxViewer) {
-			quxViewer = Quxly.Viewer(quxFile, {
+		$(window).hashchange(function () {
+			var newStateName = window.location.hash.substr(1);
+			if (newStateName.length == 0) {
+				closeModal();
+				return;
+			}
 			
-				// were to add the elements
-				container: container,
+			if (!quxViewer) {
+				quxViewer = Quxly.Viewer(quxFile, {
 				
-				// where to start
-				start: newStateName,
-				
-				// don't show the state name at the top
-				hideState: true,
-				
-				// keep the state in the history so we enable the back/forward buttons
-				afterStateChange: function (state) {
-					window.location.hash = state.name;
-				},
-				
-				// markdown converter
-				descriptionGenerator: function (descriptionArray) {
-					var html = [], minIndent = 999999999999999;
+					// were to add the elements
+					container: container,
 					
-					// find the minimum indent
-					$.each(descriptionArray, function (index, description) {
-						if (description.text.length > 0) {
-							minIndent = Math.min(minIndent, description.indentLevel);
-						}
-					});
+					// where to start
+					start: newStateName,
 					
-					// generate the plain text removing the min indent
-					$.each(descriptionArray, function (index, description) {
-						var text = [];
-						for (i = minIndent; i < description.indentLevel; i++) {
-							text.push(" ");
-						}
-						text.push(description.text);
-						html.push(text.join(""));
-					});
+					// don't show the state name at the top
+					hideState: true,
 					
-					return converter.makeHtml(html.join("\n"));
+					// keep the state in the history so we enable the back/forward buttons
+					afterStateChange: function (state) {
+						window.location.hash = state.name;
+					},
+					
+					// markdown converter
+					descriptionGenerator: function (descriptionArray) {
+						var html = [], minIndent = 999999999999999;
+						
+						// find the minimum indent
+						$.each(descriptionArray, function (index, description) {
+							if (description.text.length > 0) {
+								minIndent = Math.min(minIndent, description.indentLevel);
+							}
+						});
+						
+						// generate the plain text removing the min indent
+						$.each(descriptionArray, function (index, description) {
+							var text = [];
+							for (i = minIndent; i < description.indentLevel; i++) {
+								text.push(" ");
+							}
+							text.push(description.text);
+							html.push(text.join(""));
+						});
+						
+						return converter.makeHtml(html.join("\n"));
+					}
+				});
+			}
+			
+			quxViewer.start(newStateName);
+			
+			$.modal(container, {
+				close: true,
+				overlayClose: true,
+				onClose: function () {
+					closeModal();
+					window.location.hash = "";
+					return true;
 				}
 			});
-		}
-		
-		quxViewer.start(newStateName);
-		
-		$.modal(container, {
-			close: true,
-			overlayClose: true,
-			onClose: function () {
-				closeModal();
-				window.location.hash = "";
-				return true;
-			}
 		});
-	});
-	$(window).hashchange();			
-	
-	$("#viewSource").click(function () {
-		window.location = "view-source:" + window.location.href;
-	});
+		$(window).hashchange();			
+		
+		$(".viewSource").bind("click", function () {
+			window.location = "view-source:" + window.location.href;
+			return false;
+		});
+	}
+	catch (e) {
+		alert("OOPS! " + e);
+	}
 });
