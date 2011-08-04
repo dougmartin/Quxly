b211f1da163a048e9e115c94d011f1d2f9e08437
Doug Martin <douglasemartin@gmail.com>
2011-08-04 14:04:55 -0400

    [FIXED] tests after parser stopped ignoring blank lines

--- test/test.js
+++ test/test.js
@@ -327,11 +327,12 @@ test("line2 mixin exists", function () {
 	ok(typeof quxFile.mixins.line2 != "undefined");
 });
 
-test("foo state has two line description", function () {
-	expect(3);
-	ok(quxFile.states.foo.description.length == 2);
+test("foo state has three line description", function () {
+	expect(4);
+	ok(quxFile.states.foo.description.length == 3);
 	ok(quxFile.states.foo.description[0].text == "- description line 1");
 	ok(quxFile.states.foo.description[1].text == "- description line 2");
+	ok(quxFile.states.foo.description[2].text == "");
 });
 
 module("Single state with single mixin that is used with regex", {
@@ -350,11 +351,12 @@ test("line2 mixin exists", function () {
 	ok(typeof quxFile.mixins.line2 != "undefined");
 });
 
-test("foo state has two line description", function () {
-	expect(3);
-	ok(quxFile.states.foo.description.length == 2);
+test("foo state has three line description", function () {
+	expect(4);
+	ok(quxFile.states.foo.description.length == 3);
 	ok(quxFile.states.foo.description[0].text == "- description line 1");
 	ok(quxFile.states.foo.description[1].text == "- description line 2");
+	ok(quxFile.states.foo.description[2].text == "");
 });
 
 module("One normal state with wildcard state", {
@@ -368,9 +370,10 @@ test("foo state exists", function () {
 	ok(typeof quxFile.states.foo != "undefined");
 });
 
-test("foo state has two line description", function () {
-	expect(3);
-	ok(quxFile.states.foo.description.length == 2);
+test("foo state has three line description", function () {
+	expect(4);
+	ok(quxFile.states.foo.description.length == 3);
 	ok(quxFile.states.foo.description[0].text == "- description line 1");
-	ok(quxFile.states.foo.description[1].text == "- description line 2");
+	ok(quxFile.states.foo.description[1].text == "");
+	ok(quxFile.states.foo.description[2].text == "- description line 2");
 });
